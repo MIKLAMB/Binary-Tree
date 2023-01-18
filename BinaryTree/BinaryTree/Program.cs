6f1f4fb2d18a31c873dcb8f270fdfd5a36e05239
Michayel Alvand <miklambbest111@gmail.com>
2023-01-18 11:30:06 +0400

    realise v1.0

---
+++ BinaryTree/BinaryTree/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = new Tree<int>();
+            tree.Add(5);
+            tree.Add(3);
+            tree.Add(7);
+            tree.Add(1);
+            tree.Add(2);
+            tree.Add(8);
+            tree.Add(6);
+            tree.Add(9);
+
+            Console.WriteLine("PreOrder`");
+            foreach (var item in tree.Preorder()) //1.Node 2.Right 3.Left 
+                Console.Write(item + ",");
+            
+            Console.ReadLine();
+
+            Console.WriteLine("PostOrder`");
+            foreach (var item in tree.PostOrder()) //1.Right 2.Left 3.Node
+                Console.Write(item + ",");
+            
+            Console.ReadLine();
+
+            Console.WriteLine("InOrder`");
+            foreach (var item in tree.Inorder()) //1.Right 2.Elm 3.Node
+                Console.Write(item + ",");
+            
+            Console.ReadLine();
+
+        }
+    }
+
+}
---
+++ BinaryTree/BinaryTree/Tree.cs
@@ -0,0 +1,112 @@
+﻿namespace BinaryTree;
+
+class Tree<T> where T : IComparable, IComparable<T>
+{
+    public Node<T> Root { get; private set; }
+    public int Count { get; private set; }//Count Elm
+
+    public void Add(T data)
+    {
+        if (Root == null)
+        {
+            Root = new Node<T>(data);
+            Count = 1;
+            return;
+        }
+
+        var current = Root;
+        Root.Add(data);
+        Count++;
+
+        if (current == null)
+        {
+            Root.Add(data);
+            Count += 1;
+        }
+    }
+    public List<T> Preorder()
+    {
+
+        if (Root == null)
+            return new List<T>();
+
+        return Preorder(Root);
+    }
+    /// <summary>
+    /// 1.Right 2.Left 3.Node
+    /// </summary>
+    /// <param name="node"></param>
+    /// <returns></returns>
+    private List<T> Preorder(Node<T> node) { 
+        var list = new List<T>();
+        if (node != null)
+        {
+            list.Add(node.Data);
+
+            if (node.Left != null)
+                list.AddRange(Preorder(node.Left));
+
+            if (node.Right != null)
+                list.AddRange(Preorder(node.Right));    
+        }
+        return list;
+    }
+    public List<T> PostOrder()
+    {
+
+        if (Root == null)
+            return new List<T>();
+        
+        return PostOrder(Root);
+    }
+    /// <summary>
+    /// 1.Left 2.Right 3.Node
+    /// </summary>
+    /// <param name="node"></param>
+    /// <returns></returns>
+    private List<T> PostOrder(Node<T> node)
+    {
+        var list = new List<T>();
+        if (node != null)
+        {
+            if (node.Left != null)
+                list.AddRange(PostOrder(node.Left));
+            
+            if (node.Right != null)
+                list.AddRange(PostOrder(node.Right));
+
+            list.Add(node.Data);
+
+        }
+        return list;
+    }
+    public List<T> Inorder()
+    {
+
+        if (Root == null)
+            return new List<T>();
+        
+        return Inorder(Root);
+    }
+    /// <summary>
+    /// 1.Left 2.Elm 3.Node
+    /// </summary>
+    /// <param name="node"></param>
+    /// <returns></returns>
+    private List<T> Inorder(Node<T> node)
+    {
+        var list = new List<T>();
+        if (node != null)
+        {
+            if (node.Left != null)               
+                list.AddRange(Inorder(node.Left));               
+
+            list.Add(node.Data);
+
+            if (node.Right != null)
+                list.AddRange(Inorder(node.Right));                
+        }
+        return list;
+    }
+}
+
